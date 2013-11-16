87d629d98b7101be38c411e21c44b8e219c48e64
Joakim Tysseng <joakim.tysseng@bekk.no>
2013-11-16 11:19:14 +0100

    Improved scoping for status handler

--- opt/presentation/content/js/main.js
+++ opt/presentation/content/js/main.js
@@ -10,12 +10,7 @@ is updated)
 
 */
 
-/**
-TODO: 
-- Disable request logging in simple http server
-*/
 
-var status = "STARTING";
 var counterDivId = "#countdown_dashboard";
 var slideConfig;
 
@@ -23,15 +18,24 @@ function reloadPage(){
     window.location.reload(true);
 }
 
-
 function startPresentation(){
     $.ajaxSetup({cache: false});
 
     // reload page every six hours to free up memory
     setTimeout(reloadPage, 3 * 60 * 60 * 1000);
 
+    var reloadPresenter = function (){
+      console.log("Reloading presenter")
+      loadSlidesConfig();
+      loadCounterConfig();
+    };
+
+    var statusHandler = new Status({
+      success: reloadPresenter
+    });
+
     // check status every second
-    setInterval(fetchStatus, 1000);
+    setInterval(statusHandler.fetchStatus, 1000);
     loadSlidesConfig();
     startCounter();
     loadCounterConfig();
--- opt/presentation/content/js/status.js
+++ opt/presentation/content/js/status.js
@@ -1,71 +1,79 @@
-function fetchStatus(){
-  $.getJSON('/js/status.json', function(data) {
-    var updateCallback = function reloadPresenter(){
-      loadSlidesConfig();
-      loadCounterConfig();
-    };
+/**
+ * Module that fetches status from the server and displays a message box according to
+ * the current status.
+ */
+function Status(options) {
 
-    updateStatus(data.status, updateCallback);
-  });
-}
+  var status = "STARTING";
+  var success = options.success;
+
+  var that = this;
+
+  // Public method
+  this.fetchStatus = function() {
+    $.getJSON('/js/status.json', function(data) {
+      updateStatus(data.status, success);
+    });
+  };
 
 
+  // Private methods
+  function updateStatus(newStatus, success){
+    if(statusChanged(newStatus)){
+      if(status == 'LOAD_COMPLETED' && newStatus == 'NORMAL'){
+          success();
+      }
 
-function updateStatus(newStatus, updateCallback){
-  if(statusChanged(newStatus)){
-    if(status == 'LOAD_COMPLETED' && newStatus == 'NORMAL'){
-        updateCallback();
-    }
-
-    status = newStatus;
-    if(status == 'LOADING'){
-      showStatusBox("Laster nye bilder fra minnepinne");
-    } else if(status == 'NO_FILES_FOUND'){
-      showWarningBox("Minnepinnen inneholder ingen bilder. De gamle bildene vil fortsatt bli brukt");
-    } else if(status == 'LOAD_COMPLETED'){
-      showSuccessBox("Last fullf&oslash;rt","Lasting av nye bilder er fullf&oslash;rt. Det er n&aring; trygt &aring; ta ut minnepinnen. Nye bilder vises straks.");
-    } else if(status == 'ERROR'){
-      showErrorBox("Feil under innlasting", "Noe gikk galt under lasting av nye bilder. Det er usikkert om det er gamle eller nye bilder som vises. Pr&oslash;v &aring; ta ut minnepinnen og sett den inn igjen.");
-    } else if(status == 'NORMAL'){
-      hideStatusBox();
+      status = newStatus;
+      if(status == 'LOADING'){
+        showStatusBox("Laster nye bilder fra minnepinne");
+      } else if(status == 'NO_FILES_FOUND'){
+        showWarningBox("Minnepinnen inneholder ingen bilder. De gamle bildene vil fortsatt bli brukt");
+      } else if(status == 'LOAD_COMPLETED'){
+        showSuccessBox("Last fullf&oslash;rt","Lasting av nye bilder er fullf&oslash;rt. Det er n&aring; trygt &aring; ta ut minnepinnen. Nye bilder vises straks.");
+      } else if(status == 'ERROR'){
+        showErrorBox("Feil under innlasting", "Noe gikk galt under lasting av nye bilder. Det er usikkert om det er gamle eller nye bilder som vises. Pr&oslash;v &aring; ta ut minnepinnen og sett den inn igjen.");
+      } else if(status == 'NORMAL'){
+        hideStatusBox();
+      }
     }
   }
-}
 
-function statusChanged(newStatus){
-  return newStatus != status;
-}
+  function statusChanged(newStatus){
+    return newStatus != status;
+  }
 
-function showStatusBox(message){
-  new Messi(message, {
-    autoclose: 5000
-  });
-}
+  function showStatusBox(message){
+    new Messi(message, {
+      autoclose: 5000
+    });
+  }
 
-function showSuccessBox(title, message){
-  new Messi(message, {
-    title: title,
-    titleClass: "success",
-    autoclose: 7000
-  });
-}
+  function showSuccessBox(title, message){
+    new Messi(message, {
+      title: title,
+      titleClass: "success",
+      autoclose: 7000
+    });
+  }
 
-function showErrorBox(title, message){
-  new Messi(message, {
-    autoclose: 7000,
-    title: title,
-    titleClass: 'error'
-  });
-}
+  function showErrorBox(title, message){
+    new Messi(message, {
+      autoclose: 7000,
+      title: title,
+      titleClass: 'error'
+    });
+  }
 
-function showWarningBox(message){
-  new Messi(message, {
-    autoclose: 7000,
-    title: "Advarsel",
-    titleClass: 'warning'
-  });
-}
+  function showWarningBox(message){
+    new Messi(message, {
+      autoclose: 7000,
+      title: "Advarsel",
+      titleClass: 'warning'
+    });
+  }
 
-function hideStatusBox(){
-  $('#statusBox').html('');
+  function hideStatusBox(){
+    $('#statusBox').html('');
+  }
 }
